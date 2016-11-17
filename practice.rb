42c0c596eba42485fd4f348cd6ba7bfcb3eb88d8
Ally Boyd <alyssa.boyd85@gmail.com>
2016-11-17 08:41:31 -0600

    Ruby daily practice, learning operators

--- practice.rb
+++ practice.rb
@@ -19,3 +19,38 @@ unless sleepy
 else
   puts "Ok, I'm done for today"
 end
+
+# comparator/relational operators
+is_true = 2 != 3
+
+is_false = 2 == 3
+
+# boolean operators
+# boolean_1 = 77 < 78 && 77 < 77
+boolean_1 = false
+
+# boolean_2 = true && 100 >= 100
+boolean_2 = true
+
+# boolean_3 = 2**3 == 8 && 3**2 == 9
+boolean_3 = true
+
+# Or Operator
+# boolean_1 = 2**3 != 3**2 || true
+boolean_1 = true
+
+# boolean_2 = false || -10 > -9
+boolean_2 = false
+
+# boolean_3 = false || false
+boolean_3 = false
+
+#Not operator
+# boolean_1 = !true
+boolean_1 = false
+
+# boolean_2 = !true && !true
+boolean_2 = false
+
+# boolean_3 = !(700 / 10 == 70)
+boolean_3 = false
